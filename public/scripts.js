2dfabd9db151d098ff20f575653f69b97b389fd1
Gary Coltrane <gary.coltrane1@marist.edu>
2017-05-24 11:55:58 -0400

    added soem UI layout, gonna build entire UI then break it down into angular modules

---
+++ public/scripts.js
@@ -0,0 +1,3 @@
+// var app = angular.module("ShopList", []);
+//
+// app.controller("set")
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 9000;
+  port = process.env.PORT || 3000;
   mongoose = require('mongoose'),
   Item = require("./models/models"),
   bodyParser = require("body-parser");
@@ -11,6 +11,7 @@ var express = require('express'),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use('/public', express.static(__dirname + '/public'));
 
 
 var routes = require('./routes/routes');
