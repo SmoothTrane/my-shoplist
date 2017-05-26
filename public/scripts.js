83ad73b59a94300ab78a6b56d974d0038988eb03
Gary <gary.coltrane1@marist.edu>
2017-05-26 01:58:22 -0400

    fixed issue that I faced with AJax and JQuery, with A LITTLE bit of Angular code, now will move app  towards angular

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,14 @@
 var app = angular.module("ShopList", []);
-app.controller('ListController', function($scope) {
-    $scope.title = "John";
-    $scope.lastName = "Doe";
+app.controller('ItemController', function($scope, $http) {
+    $http({
+      method: 'GET',
+      url: '/items'
+    }).then(function successCallback(data) {
+        $scope.items = data.data;
+        
+      }, function errorCallback(data) {
+          console.log('Error: ' + data);
+
+  });
+
 });
