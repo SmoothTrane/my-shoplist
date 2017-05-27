5660101569c25125dfd225448592d332b253296f
Gary <gary.coltrane1@marist.edu>
2017-05-27 01:29:34 -0400

    fixed angular js concerns, have to update the UI

--- public/scripts.js
+++ public/scripts.js
@@ -3,12 +3,26 @@ app.controller('ItemController', function($scope, $http) {
     $http({
       method: 'GET',
       url: '/items'
-    }).then(function successCallback(data) {
-        $scope.items = data.data;
-        
+    }).then(function successCallback(response) {
+        $scope.items = response.data;
+
       }, function errorCallback(data) {
           console.log('Error: ' + data);
 
   });
 
+
+
+
+
+
 });
+
+
+
+$(function(){
+
+  $(".add-btn").click(function(){
+    $('.ui.modal').modal('show');
+  });
+})
