c398ed1128766bfe88c1859eb283801f71d589e5
Gary Coltrane <coltranegary@gmail.com>
2017-06-09 15:17:04 -0400

    testing

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express'),
   bodyParser = require("body-parser"),
   db = require("./db");
 var localDB = 'mongodb://localhost/ShopListDb'
-  mongoose.connect(db);
+  mongoose.connect(localDB);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
